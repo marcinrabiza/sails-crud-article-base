014440e4d0311ca364f8812c33e2c48a4559e096
Marcin Rabiza <marcin.rabiza@gmail.com>
2020-05-14 13:00:54 +0200

    Code cleaning

--- config/routes.js
+++ config/routes.js
@@ -19,13 +19,13 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
  
- // A MESSAGE TO MYSELF: Routes must point out not some views or actions2, but certain Controller functions such as CREATE or LIST!
+ // Routes must point out not some views or actions2, but certain Controller operations such as CREATE or LIST!
 
  '/': { view: 'pages/homepage' },
  '/articles/list': 'ArticlesController.list',
  '/articles/add': { view: 'pages/add' },
- '/articles/create': 'ArticlesController.create',
- '/articles/delete/:id' : 'ArticlesController.delete',  // Pass ID with :id 
+ '/articles/create': 'ArticlesController.create', 
+ '/articles/delete/:id' : 'ArticlesController.delete',  // Pass ID with :id
  '/articles/edit/:id' : 'ArticlesController.edit',
  '/articles/update/:id' : 'ArticlesController.update'
 
